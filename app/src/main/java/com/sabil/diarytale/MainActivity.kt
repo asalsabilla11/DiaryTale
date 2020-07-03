47a3d6c11e63db6f74010d27b915c47649e81e3d
annisa salsabilla <annisasalsa27@gmail.com>
2020-07-03 23:07:07 +0700

    membuat nav drawer

--- app/src/main/java/com/sabil/diarytale/MainActivity.kt
+++ app/src/main/java/com/sabil/diarytale/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
